7d946429b9a5d372603d09b09e139d889c715f2b
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-06-28 16:30:59 +0200

    feat: add eventListeners
    loop through all the buttons and add eventListeners that listens
    to mouse clicks for each button to play the corresponding sound

--- index.js
+++ index.js
@@ -27,3 +27,10 @@ function playSound(button){
         }
         audio.play();
 }
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+for(var i = 0 ; i < numberOfButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        playSound(this.innerHTML);
+        
+})}
